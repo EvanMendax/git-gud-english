ffd2eef85384ee59b53b26b80b1cacfee845b7e0
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2023-05-19 18:47:02 +0300

    Update MonthTrainer.tsx

--- src/Exercises/MonthTrainer/MonthTrainer.tsx
+++ src/Exercises/MonthTrainer/MonthTrainer.tsx
@@ -1,36 +1,134 @@
 import * as React from 'react';
 import Typography from "@mui/material/Typography";
-import {TextField} from "@mui/material";
+import {Checkbox, FormControlLabel, Paper, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import {useState} from "react";
 
+const questions = [
+    {
+        question: 'First month of year?',
+        engAnswer: ['january', 'January'],
+        uaAnswer: ['Січень', 'січень'],
+        engSeason: ['Winter', 'winter']
+    },
+    {
+        question: 'Second month of year?',
+        engAnswer: ['February', 'february'],
+        uaAnswer: ['лютий', 'Лютий'],
+        engSeason: ['Winter', 'winter']
+    }, {
+        question: 'Third month of year?',
+        engAnswer: ['march', 'March'],
+        uaAnswer: ['Березень', 'березень'],
+        engSeason: ['spring', 'Spring']
+    },
+    {
+        question: 'Fourth month of year?',
+        engAnswer: ['april', 'April'],
+        uaAnswer: ['Квітень', 'квітень'],
+        engSeason: ['spring', 'Spring']
+    }, {
+        question: 'Fifth month of year?',
+        engAnswer: ['may', 'May'],
+        uaAnswer: ['Травень', 'травень'],
+        engSeason: ['spring', 'Spring']
+    },
+    {
+        question: 'Sixth month of year?',
+        engAnswer: ['june', 'June'],
+        uaAnswer: ['Червень', 'червень'],
+        engSeason: ['summer', 'Summer']
+    }, {
+        question: 'Seventh month of year?',
+        engAnswer: ['july', 'July'],
+        uaAnswer: ['Липень', 'липень'],
+        engSeason: ['summer', 'Summer']
+    },
+    {
+        question: 'Eighth month of year?',
+        engAnswer: ['august', 'August'],
+        uaAnswer: ['Серпень', 'серпень'],
+        engSeason: ['summer', 'Summer']
+    }, {
+        question: 'Ninth month of year?',
+        engAnswer: ['september', 'September'],
+        uaAnswer: ['Вересень', 'вересень'],
+        engSeason: ['Autumn', 'Fall', 'autumn', 'fall']
+    },
+    {
+        question: 'Tenth month of year?',
+        engAnswer: ['october', 'October'],
+        uaAnswer: ['Жовтень', 'жовтень'],
+        engSeason: ['Autumn', 'Fall', 'autumn', 'fall']
+    }, {
+        question: 'Eleventh month of year?',
+        engAnswer: ['november', 'November'],
+        uaAnswer: ['листопад', 'Листопад'],
+        engSeason: ['Autumn', 'Fall', 'autumn', 'fall']
+    },
+    {
+        question: 'Twelfth month of year?',
+        engAnswer: ['december', 'December'],
+        uaAnswer: ['лютий', 'Лютий'],
+        engSeason: ['Winter', 'winter']
+    },
+]
 
 const MonthTrainer = () => {
 
-    const questions = [
-        {question: 'First month of year?', engAnswer: ['january', 'January'], uaAnswer: ['Січень', 'січень']},
-        {question: '1First month of year?', engAnswer: ['january', 'January'], uaAnswer: ['Січень', 'січень']},
-    ]
-    const randomQuestion = Math.floor(Math.random() * questions.length);
+    const [isRandom, setIsRandom] = useState(false)
+    const handleIsRandom = () => setIsRandom(!isRandom)
 
-    const checkResult = () => {
-
+    const shuffleQuestion = (array: any[]) => {
+        const temporaryArray = [...array]
+        for (let i = temporaryArray.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [temporaryArray[i], temporaryArray[j]] = [temporaryArray[j], temporaryArray[i]];
+        }
+        return temporaryArray
     }
 
+
     return (
-        <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column', p: 4}}>
-            <Typography sx={{fontSize: 30, fontWeight: 600, pb: 1}}>
-                {questions[randomQuestion].question}
-            </Typography>
-            <Box sx={{m: 1, height: 80}}>
-                <TextField label='In English' variant='outlined' helperText={'1'}/>
-            </Box>
-            <Box sx={{m: 1, height: 80}}>
-                <TextField label='In Ukrainian' variant='outlined' error helperText={'1'} />
-            </Box>
+        <Box sx={{display: 'grid', gridTemplateColumns: '1fr 4fr'}}>
             <Box>
-                <Button variant='contained' sx={{color: 'white', bgcolor: 'black', m: 1}}>Check</Button>
-                <Button disabled variant='contained' sx={{color: 'white', bgcolor: 'black', m: 1}}>Next</Button>
+                <Paper sx={{
+                    bgcolor: '#dce1f8',
+                    borderRadius: 2,
+                    p: 2,
+                    mt: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Typography sx={{fontSize: 25, mb:1}}>Step 1/12</Typography>
+                    <FormControlLabel sx={{mb: 1}} control={
+                        <Checkbox sx={{'& .MuiSvgIcon-root': {fontSize: 28}}}
+                                  onClick={handleIsRandom} checked={isRandom}/>
+                    } label="Random"/>
+                    <Button variant='contained' sx={{color: 'white', bgcolor: 'black', m: 1}}>Restart</Button>
+                </Paper>
+            </Box>
+            <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column', p: 4}}>
+                <Typography sx={{fontSize: 30, fontWeight: 600, pb: 1}}>
+                    {questions[0].question}
+                </Typography>
+                <Box sx={{m: 1, height: 80}}>
+                    <TextField label='In English' variant='outlined' helperText={'1'}/>
+                </Box>
+                <Box sx={{m: 1, height: 80}}>
+                    <TextField label='In Ukrainian' variant='outlined' error helperText={'1'}/>
+                </Box>
+                <Box sx={{m: 1, height: 80}}>
+                    <TextField label='Season (eng)' variant='outlined' error helperText={'1'}/>
+                </Box>
+                <Box>
+                    <Button onClick={() => shuffleQuestion(questions)} variant='contained'
+                            sx={{color: 'white', bgcolor: 'black', m: 1}}>Check</Button>
+                    <Button variant='contained'
+                            sx={{color: 'white', bgcolor: 'black', m: 1}}>Next</Button>
+                </Box>
             </Box>
         </Box>
     );
